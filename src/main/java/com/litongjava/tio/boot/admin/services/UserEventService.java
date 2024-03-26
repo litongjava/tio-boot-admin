50e10c75b98f1a01dfd400c57154358a195b0137
litongjava <litongjava@qq.com>
2024-03-25 21:40:26 -1000

    add SnowflakeIdGenerator

--- src/main/java/com/litongjava/tio/boot/admin/services/UserEventService.java
+++ src/main/java/com/litongjava/tio/boot/admin/services/UserEventService.java
@@ -1,6 +1,7 @@
 package com.litongjava.tio.boot.admin.services;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.litongjava.data.utils.SnowflakeIdGenerator;
 import com.litongjava.jfinal.plugin.activerecord.Db;
 import com.litongjava.jfinal.plugin.activerecord.Record;
 import com.litongjava.tio.boot.admin.costants.TableNames;
@@ -18,11 +19,21 @@ public class UserEventService {
     String jsonString = eventValue.toJSONString();
     PGobject pGobject = new PGobject();
     pGobject.setType("json");
+    long threadId = Thread.currentThread().getId();
+    if (threadId > 31) {
+      threadId = threadId % 31;
+    }
+    if (threadId < 0) {
+      threadId = 0;
+    }
+    long id = new SnowflakeIdGenerator(threadId, 0).generateId();
+
     try {
       pGobject.setValue(jsonString);
       Record record = new Record();
       record.set("name", eventName);
       record.set("value", pGobject);
+      record.set("id", id);
       boolean save = Db.save(TableNames.tio_boot_admin_system_user_event, record);
       log.info("save result:{}", save);
     } catch (SQLException e) {
