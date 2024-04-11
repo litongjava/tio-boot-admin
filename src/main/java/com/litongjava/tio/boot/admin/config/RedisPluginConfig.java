eea818b766dcc157ac1fe6b3771a6d0b479e07af
litongjava <litongjava@qq.com>
2024-04-10 20:54:25 -1000

    update redis config

--- src/main/java/com/litongjava/tio/boot/admin/config/RedisPluginConfig.java
+++ src/main/java/com/litongjava/tio/boot/admin/config/RedisPluginConfig.java
@@ -7,14 +7,20 @@ import com.litongjava.jfinal.plugin.redis.Cache;
 import com.litongjava.jfinal.plugin.redis.Redis;
 import com.litongjava.jfinal.plugin.redis.RedisPlugin;
 import com.litongjava.tio.boot.server.TioBootServer;
+import com.litongjava.tio.utils.environment.EnvironmentUtils;
 
 @AConfiguration
 public class RedisPluginConfig {
 
   @AInitialization
   public RedisPlugin redisPlugin() {
+    String host = EnvironmentUtils.getStr("redis.host");
+    Integer port = EnvironmentUtils.getInt("redis.port");
+    String password = EnvironmentUtils.getStr("redis.password");
+    String cacheName = EnvironmentUtils.get("redis.cacheName");
+
     // 创建并启动 Redis 插件
-    RedisPlugin bbsRedis = new RedisPlugin("main", "localhost");
+    RedisPlugin bbsRedis = new RedisPlugin(cacheName, host, port, password);
     bbsRedis.start();
 
     // 测试连接
