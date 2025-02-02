08338793484b01fffae4e8658e9dcd1b60423c0c
litongjava <litongjava@qq.com>
2025-02-01 20:45:15 -1000

    update upload reusltVo

--- src/main/java/com/litongjava/tio/boot/admin/vo/UploadResultVo.java
+++ src/main/java/com/litongjava/tio/boot/admin/vo/UploadResultVo.java
@@ -12,12 +12,27 @@ import lombok.experimental.Accessors;
 public class UploadResultVo {
   private Long id;
   private Long size;
-  private String filename, targetName, url, md5;
+  private String name, targetName, url, md5;
 
   public UploadResultVo(long id, String filename, Long size, String url, String md5) {
     this.id = id;
-    this.filename = filename;
+    this.name = filename;
     this.size = size;
+    this.url = url;
+    this.md5 = md5;
+  }
+  
+  public UploadResultVo(long id, String filename, String url, String md5) {
+    this.id = id;
+    this.name = filename;
+    this.url = url;
+    this.md5 = md5;
+  }
+
+  public UploadResultVo(long id, String filename, String targetName, String url, String md5) {
+    this.id = id;
+    this.name = filename;
+    this.targetName = targetName;
     this.url = url;
     this.md5 = md5;
   }
