7f9d2085aec9821e67d530b4db6b5af441ee9679
Tong Li <litongjava001@gmail.com>
2025-07-16 23:48:48 -1000

    update GlobalExceptionHadler

--- src/main/java/com/litongjava/tio/boot/admin/handler/GlobalExceptionHadler.java
+++ src/main/java/com/litongjava/tio/boot/admin/handler/GlobalExceptionHadler.java
@@ -1,21 +1,13 @@
 package com.litongjava.tio.boot.admin.handler;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.time.ZonedDateTime;
-import java.util.Map;
-
-import com.litongjava.constants.ServerConfigKeys;
 import com.litongjava.model.body.RespBodyVo;
-import com.litongjava.tio.boot.agent.NotificationSender;
 import com.litongjava.tio.boot.exception.TioBootExceptionHandler;
-import com.litongjava.tio.boot.http.TioRequestContext;
+import com.litongjava.tio.boot.sender.NotifactionWarmUtils;
+import com.litongjava.tio.boot.sender.NotificationSender;
 import com.litongjava.tio.boot.server.TioBootServer;
 import com.litongjava.tio.core.ChannelContext;
 import com.litongjava.tio.http.common.HttpRequest;
-import com.litongjava.tio.http.common.utils.HttpIpUtils;
 import com.litongjava.tio.utils.environment.EnvUtils;
-import com.litongjava.tio.utils.network.IpUtils;
 import com.litongjava.tio.utils.notification.NotifactionWarmModel;
 import com.litongjava.tio.websocket.common.WebSocketRequest;
 
@@ -26,41 +18,20 @@ public class GlobalExceptionHadler implements TioBootExceptionHandler {
 
   @Override
   public RespBodyVo handler(HttpRequest request, Throwable e) {
-    String requestId = request.getChannelContext().getId();
-
-    String requestLine = request.getRequestLine().toString();
-    String host = request.getHost();
-    Map<String, String> headers = request.getHeaders();
-    String bodyString = request.getBodyString();
-
-    String realIp = HttpIpUtils.getRealIp(request);
-
-    // 获取完整的堆栈跟踪
-    StringWriter sw = new StringWriter();
-    PrintWriter pw = new PrintWriter(sw);
-    e.printStackTrace(pw);
-    String stackTrace = sw.toString();
-
-    log.info("requestId,{},{},{},{},{}", requestId, host, requestLine, headers, bodyString, stackTrace);
-    NotifactionWarmModel model = new NotifactionWarmModel();
-
-    String localIp = IpUtils.getLocalIp();
-    model.setAppEnv(EnvUtils.env());
-    model.setAppGroupName("tio-boot");
-    model.setAppName(EnvUtils.get(ServerConfigKeys.APP_NAME));
-    model.setWarningName("GlobalExceptionHadler");
-    model.setLevel("LeveL 1");
-
-    model.setDeviceName(localIp);
-    model.setTime(ZonedDateTime.now());
-    model.setRequestId(requestId);
-    model.setUserIp(realIp);
-    model.setUserId(TioRequestContext.getUserIdString());
-    model.setHost(host);
-    model.setRequestLine(requestLine);
-    model.setRequestBody(bodyString);
-    model.setStackTrace(stackTrace);
+    
+    String appGroupName = "tio-boot";
+    String warningName = "GlobalExceptionHadler";
+    String level = "LeveL 1";
+    
+    NotifactionWarmModel model = NotifactionWarmUtils.toWarmModel(appGroupName, warningName, level, request, e);
+    String requestId = model.getRequestId();
+    String host = model.getHost();
+    String requestLine = model.getRequestLine();
+    String bodyString = model.getRequestBody();
+    String stackTrace = model.getStackTrace();
+    log.info("requestId,{},{},{},{}", requestId, host, requestLine, bodyString, stackTrace);
     model.setContent("unknow error");
+    
 
     if (!EnvUtils.isDev()) {
       NotificationSender notificationSender = TioBootServer.me().getNotificationSender();
@@ -71,6 +42,7 @@ public class GlobalExceptionHadler implements TioBootExceptionHandler {
 
     return RespBodyVo.fail(e.getMessage());
   }
+
 
   @Override
   public Object wsTextHandler(WebSocketRequest webSokcetRequest, String text, ChannelContext channelContext, HttpRequest httpRequest, Throwable e) {
