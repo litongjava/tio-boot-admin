7be7dce6390998c57e45581ebf8e0613b77b961b
Tong Li <litongjava001@gmail.com>
2025-08-31 20:49:42 -1000

    add TioBootAdminEnjoyEngineConfig

---
+++ src/main/java/com/litongjava/tio/boot/admin/config/TioBootAdminEnjoyEngineConfig.java
@@ -0,0 +1,25 @@
+package com.litongjava.tio.boot.admin.config;
+
+import com.jfinal.template.Engine;
+import com.litongjava.template.RowFieldGetter;
+
+public class TioBootAdminEnjoyEngineConfig {
+
+  private final String RESOURCE_BASE_PATH = "/enjoy-templates/";
+
+  public void config() {
+    Engine engine = Engine.use();
+    engine.setBaseTemplatePath(RESOURCE_BASE_PATH);
+    engine.setToClassPathSourceFactory();
+    // 开启模板热加载，大部分生产环境建议开启，除非追求极致性能
+    engine.setDevMode(true);
+    // 配置极速模式，性能提升 13%
+    Engine.setFastMode(true);
+    // 支持中文表达式、中文变量名、中文方法名及中文模板函数名
+    Engine.setChineseExpression(true);
+
+    // add RowFieldGetter
+    Engine.addFieldGetterToFirst(new RowFieldGetter());
+
+  }
+}
