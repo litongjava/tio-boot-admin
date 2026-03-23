15f2629183e3956da9467cdd5b7cf2d62892e1b5
litongjava <litongjava001@gmail.com>
2026-03-23 23:36:54 +0800

    add STORAGE_PLATFORM

--- src/main/java/com/litongjava/tio/boot/admin/costants/AppConstant.java
+++ src/main/java/com/litongjava/tio/boot/admin/costants/AppConstant.java
@@ -1,7 +1,8 @@
 package com.litongjava.tio.boot.admin.costants;
 
 public interface AppConstant {
-
+  String STORAGE_PLATFORM = "storage.platform";
+  
   String ADMIN_SECRET_KEY = "admin.secret.key";
   String ADMIN_TOKEN = "admin.token";
   String APP_TOKEN_TIMEOUT = "app.token.timeout";
---
+++ src/main/java/com/litongjava/tio/boot/admin/services/storage/UniStorageService.java
@@ -0,0 +1,159 @@
+package com.litongjava.tio.boot.admin.services.storage;
+
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.StrKit;
+import com.litongjava.db.TableInput;
+import com.litongjava.db.TableResult;
+import com.litongjava.db.activerecord.Row;
+import com.litongjava.jfinal.aop.Aop;
+import com.litongjava.model.body.RespBodyVo;
+import com.litongjava.model.upload.UploadFile;
+import com.litongjava.model.upload.UploadResult;
+import com.litongjava.table.services.ApiTable;
+import com.litongjava.tio.boot.admin.consts.StoragePlatformConst;
+import com.litongjava.tio.boot.admin.costants.TioBootAdminTableNames;
+import com.litongjava.tio.boot.admin.dao.SystemUploadFileDao;
+import com.litongjava.tio.boot.admin.services.StorageService;
+import com.litongjava.tio.boot.admin.services.system.SystemUploadFileService;
+import com.litongjava.tio.boot.admin.utils.AwsS3Utils;
+import com.litongjava.tio.utils.crypto.Md5Utils;
+import com.litongjava.tio.utils.hutool.FilenameUtils;
+import com.litongjava.tio.utils.snowflake.SnowflakeIdUtils;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+@Slf4j
+public class UniStorageService implements StorageService {
+  @Override
+  public RespBodyVo upload(UploadFile uploadFile) {
+    return upload(DEFAULT_CATEGORY, uploadFile);
+  }
+
+  public RespBodyVo upload(String category, UploadFile uploadFile) {
+    if (StrKit.isBlank(category)) {
+      category = DEFAULT_CATEGORY;
+    }
+    UploadResult uploadResultVo = uploadFile(category, uploadFile);
+
+    return RespBodyVo.ok(uploadResultVo);
+  }
+
+  public UploadResult uploadFile(String category, UploadFile uploadFile) {
+    long id = SnowflakeIdUtils.id();
+    return uploadFile(category, uploadFile, id);
+  }
+
+  public UploadResult uploadFile(String category, UploadFile uploadFile, Long id) {
+    // 上传文件
+    String name = uploadFile.getName();
+    String suffix = FilenameUtils.getSuffix(name);
+    String newFilename = id + "." + suffix;
+    String targetName = category + "/" + newFilename;
+    return uploadFile(id, targetName, uploadFile, suffix);
+  }
+
+  /**
+   * @param id
+   * @param originFilename
+   * @param targetName
+   * @param fileContent
+   * @param size
+   * @param suffix
+   * @return
+   */
+  public UploadResult uploadFile(long id, String targetName, UploadFile uploadFile, String suffix) {
+    String name = uploadFile.getName();
+    long size = uploadFile.getSize();
+    byte[] fileContent = uploadFile.getData();
+
+    String md5 = Md5Utils.md5Hex(fileContent);
+    Row record = Aop.get(SystemUploadFileDao.class).getFileBasicInfoByMd5(md5);
+    if (record != null) {
+      log.info("select table reuslt:{}", record.toMap());
+      id = record.getLong("id");
+      String url = this.getUrl(record.getStr("bucket_name"), record.getStr("target_name"));
+      Kv kv = record.toKv();
+      kv.remove("target_name");
+      kv.remove("bucket_name");
+      kv.set("url", url);
+      kv.set("md5", md5);
+      return new UploadResult(id, name, size, url, md5);
+    } else {
+      log.info("not found from cache table:{}", md5);
+    }
+
+    String etag = null;
+    try (S3Client client = AwsS3Utils.buildClient();) {
+      PutObjectResponse response = AwsS3Utils.upload(client, AwsS3Utils.bucketName, targetName, fileContent, suffix);
+      etag = response.eTag();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+
+    // Log and save to database
+    log.info("Uploaded with ETag: {}", etag);
+
+    TableInput kv = TableInput.create().set("name", name).set("size", size).set("md5", md5)
+        //
+        .set("platform", StoragePlatformConst.aws_s3).set("region_name", AwsS3Utils.regionName)
+        .set("bucket_name", AwsS3Utils.bucketName)
+        //
+        .set("target_name", targetName).set("file_id", etag);
+
+    TableResult<Kv> save = ApiTable.save(TioBootAdminTableNames.tio_boot_admin_system_upload_file, kv);
+    String downloadUrl = getUrl(AwsS3Utils.bucketName, targetName);
+
+    return new UploadResult(save.getData().getLong("id"), name, Long.valueOf(size), downloadUrl, md5);
+
+  }
+
+  @Override
+  public String getUrl(String bucketName, String targetName) {
+    return AwsS3Utils.getUrl(bucketName, targetName);
+  }
+
+  @Override
+  public String getUrl(String targetName) {
+    return AwsS3Utils.getUrl(targetName);
+  }
+
+  @Override
+  public UploadResult getUrlById(String id) {
+    return Aop.get(SystemUploadFileService.class).getUrlById(id);
+  }
+
+  @Override
+  public UploadResult getUrlById(long id) {
+    return Aop.get(SystemUploadFileService.class).getUrlById(id);
+  }
+
+  @Override
+  public UploadResult getUrlByMd5(String md5) {
+    return Aop.get(SystemUploadFileService.class).getUrlByMd5(md5);
+  }
+
+  @Override
+  public String getPresignedDownloadUrl(String targetName) {
+
+    return AwsS3Utils.getPresignedDownloadUrl(targetName);
+  }
+
+  @Override
+  public String getPresignedDownloadUrl(String bucket, String targetName) {
+    return AwsS3Utils.getPresignedDownloadUrl(bucket, targetName);
+  }
+
+  @Override
+  public String getPresignedDownloadUrl(String region, String bucket, String targetName) {
+    return AwsS3Utils.getPresignedDownloadUrl(region, bucket, targetName);
+  }
+
+  @Override
+  public UploadResult getPresignedDownloadUrl(Long id) {
+    return Aop.get(SystemUploadFileService.class).getPresignedDownloadUrl(id);
+  }
+
+}
